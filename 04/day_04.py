81c10e027cf785a15dd4a5dba23ff30f14c5c513
Cassandra Luong <cassandraluong@gmail.com>
2023-12-03 23:07:25 -0800

    Optimize AoC Day 4

--- 04/day_04.py
+++ 04/day_04.py
@@ -8,14 +8,8 @@ with open("input.txt", "r") as input:
     scratchcards = scratchcards.splitlines()
 
 
-#
 def remove_whitespace(input_list):
-    strnum_list = input_list
-    new_list = []
-    for strnum in strnum_list:
-        if strnum != "":
-            new_list.append(strnum.strip())
-    return new_list
+    return [num.strip() for num in input_list if num != ""]
 
 
 ###########
@@ -46,14 +40,13 @@ print(total_points_all_scratchcards)
 # Part Two
 ###########
 total_scratchcards = 0
-scratchcard_dict = {}
+scratchcard_counts = {}
+all_matching_numbers = {}
 
 for scratchcard in scratchcards:
     card = int(scratchcard.split(": ")[0].split(" ")[-1])
-    scratchcard_dict[card] = 1
+    scratchcard_counts[card] = 1
 
-for scratchcard in scratchcards:
-    card = int(scratchcard.split(": ")[0].split(" ")[-1])
     all_numbers = scratchcard.split(": ")[1].split(" | ")
     winning_numbers = remove_whitespace(all_numbers[0].split(" "))
     elf_numbers = remove_whitespace(all_numbers[1].split(" "))
@@ -61,14 +54,15 @@ for scratchcard in scratchcards:
     winning_numbers_set = set(winning_numbers)
     elf_numbers_set = set(elf_numbers)
     matching_numbers = winning_numbers_set & elf_numbers_set
+    all_matching_numbers[card] = matching_numbers
 
-    if len(matching_numbers) > 0:
-        i = 1
-        for index, num in enumerate(matching_numbers):
-            if scratchcard_dict.get(card + i) is None:
-                pass
-            else:
-                scratchcard_dict[card + i] += scratchcard_dict.get(card)
-                i += 1
+for scratchcard in scratchcards:
+    card = int(scratchcard.split(": ")[0].split(" ")[-1])
 
-print(sum(scratchcard_dict.values()))
+    for i, num in enumerate(all_matching_numbers.get(card)):
+        if scratchcard_counts.get(card + i + 1) is None:
+            pass
+        else:
+            scratchcard_counts[card + i + 1] += scratchcard_counts.get(card)
+
+print(sum(scratchcard_counts.values()))
