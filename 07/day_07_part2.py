5d94d85026384d68af7c8448fc0cfa3d9d176238
Cassandra Luong <cassandraluong@gmail.com>
2023-12-21 12:04:55 -0500

    Complete AoC 2023 Day 7, Part Two

--- 07/day_07_part2.py
+++ 07/day_07_part2.py
@@ -3,7 +3,7 @@ Advent of Code - Day 7: Camel Cards
 """
 
 ###########
-# Part One
+# Part Two
 ###########
 
 with open("input.txt", "r") as input:
@@ -11,10 +11,9 @@ with open("input.txt", "r") as input:
     puzzle_input = puzzle_input.splitlines()
 
 card_values = {
-    "A": 14,
-    "K": 13,
-    "Q": 12,
-    "J": 11,
+    "A": 13,
+    "K": 12,
+    "Q": 11,
     "T": 10,
     "9": 9,
     "8": 8,
@@ -24,6 +23,7 @@ card_values = {
     "4": 4,
     "3": 3,
     "2": 2,
+    "J": 1,
 }
 types = {
     "five_of_a_kind": 7,
@@ -49,21 +49,41 @@ for item in puzzle_input:
     match number_unique_cards:
         case 1:
             hand_type, strength = "five_of_a_kind", 7
-
         case 2:
             if max_count == 4:
-                hand_type, strength = "four_of_a_kind", 6
+                if "J" in hand:
+                    hand_type, strength = "five_of_a_kind", 7
+                else:
+                    hand_type, strength = "four_of_a_kind", 6
             else:
-                hand_type, strength = "full_house", 5
+                if "J" in hand:
+                    hand_type, strength = "five_of_a_kind", 7
+                else:
+                    hand_type, strength = "full_house", 5
         case 3:
             if max_count == 3:
+                if "J" in hand:
+                    hand_type, strength = "four_of_a_kind", 6
+                else:
+                    hand_type, strength = "three_of_a_kind", 4
+            else:
+                if "J" in hand:
+                    if hand.count("J") == 1:
+                        hand_type, strength = "full_house", 5
+                    else:
+                        hand_type, strength = "four_of_a_kind", 6
+                else:
+                    hand_type, strength = "two_pair", 3
+        case 4:
+            if "J" in hand:
                 hand_type, strength = "three_of_a_kind", 4
             else:
-                hand_type, strength = "two_pair", 3
-        case 4:
-            hand_type, strength = "one_pair", 2
+                hand_type, strength = "one_pair", 2
         case _:
-            hand_type, strength = "high_card", 1
+            if "J" in hand:
+                hand_type, strength = "one_pair", 2
+            else:
+                hand_type, strength = "high_card", 1
 
     all_hands[hand] = {"bid": bid, "hand_type": hand_type, "strength": strength}
 
